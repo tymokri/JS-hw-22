a64294efe85d84a993ffda02bb00a54d6246c6a8
Kristina Tymofiichuk <99436996+tymokri@users.noreply.github.com>
2022-08-11 23:44:51 +0100

    Added lodash, eslint

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-"use strict";
+'use strict';
 
-// dgyt
+function sum(a, b) {
+  return a + b;
+}
+
+sum(5, 10);
+
+const sum2 = (a, b) => a + b;
+sum2(10, 20);
+
+function sub(a, b) {
+  return a - b;
+}
+
+sub(5, 10);
+
+const mult = (a, b) => a * b;
+mult(3, 8);
